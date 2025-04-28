250371709f2d4706bde4c9fd7f40c87fd166bfd2
Prince Singh <imprincestark.45@gmail.com>
2025-04-28 13:40:29 +0530

    database connection

---
+++ src/index.js
@@ -0,0 +1,52 @@
+//require('dotenv').config({path : './env'}) ; 
+import dotenv from 'dotenv' ;
+import mongoose from "mongoose";
+import {DB_NAME} from './constants.js' ;
+import express from 'express'
+import connectDB from "./db/index.js";
+
+dotenv.config({path :'./env'})
+
+
+ connectDB()
+
+
+
+
+
+
+
+
+
+
+
+/*
+//1approch
+
+
+const app = express() ; 
+
+
+// iife used here 
+
+;(async ()=> {
+    try{
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`) ; 
+    app.on('error' , (error) => {
+        console.log('ERRR' , error) ; 
+        throw error ; 
+    })
+
+     app.listen(process.env.PORT , () => {
+        console.log(`Server is running on port ${process.env.PORT}`) ;
+     }) 
+    }catch(err) {
+        console.log(err);
+        throw err ; 
+    }
+})() ; 
+
+function connectDB() {
+     
+}
+*/
